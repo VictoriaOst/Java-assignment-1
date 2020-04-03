548557e882efc4a3ef8cd19939c1eb1b5f83a792
Viktoria <45072248+VictoriaOst@users.noreply.github.com>
2020-04-03 19:16:31 +0300

    init assignment-1

---
+++ laba1/src/main/java/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+import static com.company.WordsCalculation.wordsWithDiffChars;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String str = "fef";
+        String lpl = str;
+        System.out.println(WordsCalculation.wordsWithDiffChars("Hello fogen ктукщт hk"));
+    }
+}
